3fdde1df8d4962290e90c63e4d5f1b720100ffa0
furkan taşdemir <118552824+tekelfurkan@users.noreply.github.com>
2022-11-19 11:37:07 +0300

    forloop
    1den 10 kadar olan sayıları yazdırır

---
+++ git_hub.java
@@ -0,0 +1,10 @@
+package days.day22;
+
+public class git_hub {
+    public static void main(String[] args) {
+    for (int ı = 1; ı <=10; ı++ )
+    {
+        System.out.println(ı);
+    }
+    }
+}
